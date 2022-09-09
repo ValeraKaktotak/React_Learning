0f7951483e47705f84f9e290967ea7dd66b2e46e
Valera <valerakaktotak@gmail.com>
2022-09-09 17:53:52 +0300

    Investigated components

---
+++ my-app/src/Header.js
@@ -0,0 +1,15 @@
+
+const Header = () =>{
+    return(
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
