9a82c6258ef6d68ef007f4374c805ad16b99d6f3
Valera <valerakaktotak@gmail.com>
2022-09-09 12:26:00 +0300

    create default react app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>
+        hello world!!!!!
+      </p>
+    </div>
+  );
+}
+
+export default App;
