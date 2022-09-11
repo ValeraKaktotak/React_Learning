7496672b1aec0e034dc0a33afac74fc4bca355ca
Valera <valerakaktotak@gmail.com>
2022-09-11 11:29:16 +0300

    add new components

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <img src="https://assets.turbologo.ru/blog/ru/2019/04/18165419/oracle.png" alt="sky"/>
+        </header>
+    );
+}
+
+export default Header
