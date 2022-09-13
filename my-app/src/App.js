5ef940d36039a7e40b089a3cb20278d0708eb821
Valera <valerakaktotak@gmail.com>
2022-09-13 11:55:45 +0300

    add react-router-dom and create routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className="App_wrapper">
-            <Header/>
-            <Sidebar/>
-            <div className="App_wrapper_content">
-                {/*<Profile/>*/}
-                <Dialogs />
+        <BrowserRouter>
+            <div className="App_wrapper">
+                <Header/>
+                <Sidebar/>
+                <div className="App_wrapper_content">
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>} />
+                        <Route path="/dialogs" element={<Dialogs/>} />
+                        <Route path="/news" element={<News/>} />
+                        <Route path="/music" element={<Music/>} />
+                        <Route path="/settings" element={<Settings/>} />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
