3034252b8347d939a29d94030c9f0bdc122d4d08
Valera <valerakaktotak@gmail.com>
2022-09-13 10:31:22 +0300

    add component dialogs

---
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import style from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return(
+        <div className={style.dialogs}>
+            Dialogs
+        </div>
+    );
+}
+
+export default Dialogs
