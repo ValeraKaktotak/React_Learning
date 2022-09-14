9fcf5ed05559b3f9dc2aff3bc01b69417f49f2c0
Valera <valerakaktotak@gmail.com>
2022-09-14 13:05:06 +0300

    add BLL UI - first stap to dinamic data

--- my-app/src/components/Dialogs/UserDialogs/UserDialogs.jsx
+++ my-app/src/components/Dialogs/UserDialogs/UserDialogs.jsx
@@ -2,13 +2,20 @@ import style from './UserDialogs.module.css'
 import UserDialog from "./UserDialog/UserDialog";
 
 const UserDialogs = () => {
+    const dialogsData = [
+        {id: '1', name: 'Valera'},
+        {id: '2', name: 'Dima'},
+        {id: '3', name: 'Sveta'},
+        {id: '4', name: 'Marina'},
+        {id: '5', name: 'Lena'},
+    ]
     return(
         <div className={style.user_dialogs}>
-            <UserDialog name="Valera" id="1"/>
-            <UserDialog name="Dima" id="2"/>
-            <UserDialog name="Sveta" id="3"/>
-            <UserDialog name="Marina" id="4"/>
-            <UserDialog name="Lena" id="5"/>
+            <UserDialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+            <UserDialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+            <UserDialog name={dialogsData[2].name} id={dialogsData[2].id}/>
+            <UserDialog name={dialogsData[3].name} id={dialogsData[3].id}/>
+            <UserDialog name={dialogsData[4].name} id={dialogsData[4].id}/>
         </div>
     );
 }
--- my-app/src/components/Profile/Myposts/MyPosts.jsx
+++ my-app/src/components/Profile/Myposts/MyPosts.jsx
@@ -2,6 +2,11 @@ import style from './MyPosts.module.css';
 import MyPost from "./MyPost/MyPost";
 
 const MyPosts = () => {
+    const postData = [
+        {id: '1', message: 'Hi, it\'s my first post', likes: 21},
+        {id: '2', message: 'Hi, it\'s my second post', likes: 11},
+        {id: '3', message: 'Hi, it\'s my third post', likes: 15},
+    ]
     return(
         <div className={style.my_posts}>
             <div className={style.add_data}>
@@ -11,9 +16,9 @@ const MyPosts = () => {
                 </div>
             </div>
             <h3>My posts</h3>
-            <MyPost message = "Hi, it's my first post" likes="21" />
-            <MyPost message = "Hi, it's my second post" likes="11"/>
-            <MyPost message = "Hi, it's my third post" likes="15"/>
+            <MyPost message = {postData[0].message} likes={postData[0].likes} />
+            <MyPost message = {postData[1].message} likes={postData[1].likes}/>
+            <MyPost message = {postData[2].message} likes={postData[2].likes}/>
         </div>
     );
 }
