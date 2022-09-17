578332c45af9cbf6a2c346b0d5ad81d2080bf6bc
Valera <valerakaktotak@gmail.com>
2022-09-17 16:06:43 +0300

    add object store in state and refactoring state.js

--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -1,56 +1,80 @@
-let render;
+let store = {
 
-const state = {
-    messagesPage: {
-        dialogData: [
-            {id: 1, name: 'Valera'},
-            {id: 2, name: 'Dima'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Marina'},
-            {id: 5, name: 'Lena'},
-        ],
-        messageData: [
-            {id: 1, message: 'Hi:)'},
-            {id: 2, message: 'hi hi hi:)'},
-            {id: 3, message: 'Hello!)))'},
-            {id: 4, message: 'How are you?)'},
-            {id: 5, message: 'Are you ok?'},
-        ]
+    render() {
     },
-    profilePage: {
-        postData: [
-            {id: 1, message: 'Hi, it\'s my first post', likes: 21, avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'},
-            {id: 2, message: 'Hi, it\'s my second post', likes: 11, avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'},
-            {id: 3, message: 'Hi, it\'s my third post', likes: 15, avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'},
-        ],
-        newMessageArea: ''
+    subscriber(observer) {
+        this.render = observer;
     },
-    sidebar: {
-        friends: [
-            {id: 1, avatar: 'https://marvel.com.ru/imagecache/original/uploads/images/ued3m0s0HRwkYy6KAM1F.png'},
-            {id: 2, avatar: 'https://i.pinimg.com/736x/f5/78/b7/f578b78a53a98d9260961235f83408c1.jpg'},
-            {id: 3, avatar: 'https://images-on-off.com/images/49/fireplamyageroimarvelmarvelidccomics-673fe4c4.jpg'}
-        ]
-    }
-}
-export const addPost = () => {
-    let newPostObject = {
-        id: 4,
-        message: state.profilePage.newMessageArea,
-        likes: 0,
-        avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'
-    }
-    state.profilePage.postData.unshift(newPostObject);
-    state.profilePage.newMessageArea = '';
-    render(state);
-}
-export const changePostText = (text) => {
-    state.profilePage.newMessageArea = text;
-    render(state);
+
+    _state: {
+        messagesPage: {
+            dialogData: [
+                {id: 1, name: 'Valera'},
+                {id: 2, name: 'Dima'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Marina'},
+                {id: 5, name: 'Lena'},
+            ],
+            messageData: [
+                {id: 1, message: 'Hi:)'},
+                {id: 2, message: 'hi hi hi:)'},
+                {id: 3, message: 'Hello!)))'},
+                {id: 4, message: 'How are you?)'},
+                {id: 5, message: 'Are you ok?'},
+            ]
+        },
+        profilePage: {
+            postData: [
+                {
+                    id: 1,
+                    message: 'Hi, it\'s my first post',
+                    likes: 21,
+                    avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'
+                },
+                {
+                    id: 2,
+                    message: 'Hi, it\'s my second post',
+                    likes: 11,
+                    avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'
+                },
+                {
+                    id: 3,
+                    message: 'Hi, it\'s my third post',
+                    likes: 15,
+                    avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'
+                },
+            ],
+            newMessageArea: ''
+        },
+        sidebar: {
+            friends: [
+                {id: 1, avatar: 'https://marvel.com.ru/imagecache/original/uploads/images/ued3m0s0HRwkYy6KAM1F.png'},
+                {id: 2, avatar: 'https://i.pinimg.com/736x/f5/78/b7/f578b78a53a98d9260961235f83408c1.jpg'},
+                {id: 3, avatar: 'https://images-on-off.com/images/49/fireplamyageroimarvelmarvelidccomics-673fe4c4.jpg'}
+            ]
+        }
+    },
+    getState() {
+        return this._state;
+    },
+
+    addPost() {
+        let newPostObject = {
+            id: 4,
+            message: this._state.profilePage.newMessageArea,
+            likes: 0,
+            avatar: 'https://meragor.com/files/styles//ava_800_800_wm/avto-bmv_bmw-fon-transport-41424.jpg'
+        }
+        this._state.profilePage.postData.unshift(newPostObject);
+        this._state.profilePage.newMessageArea = '';
+        this.render(this._state);
+    },
+
+    changePostText(text) {
+        this._state.profilePage.newMessageArea = text;
+        this.render(this._state);
+    },
 }
 
-export const subscriber = (observer) => {
-    render = observer;
-}
 
-export default state
+export default store
